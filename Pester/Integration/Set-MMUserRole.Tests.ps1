d5645435cde463a05b8aa4a12548c39e7a5fe640
faithless-padre <48208316+faithless-padre@users.noreply.github.com>
2026-03-16 09:53:20 +0000

    FIX/ Local tests

--- Pester/Integration/Set-MMUserRole.Tests.ps1
+++ Pester/Integration/Set-MMUserRole.Tests.ps1
@@ -7,7 +7,14 @@ BeforeAll {
         AdminPassword = if ($env:MM_ADMIN_PASSWORD) { $env:MM_ADMIN_PASSWORD } else { $fileConfig.AdminPassword }
     }
 
-    Connect-MMServer -Url $config.Url -Username $config.AdminUsername -Password (ConvertTo-SecureString $config.AdminPassword -AsPlainText -Force)
+    $modulePath = if (Test-Path '/module/MatterMostV4.psd1') {
+        '/module/MatterMostV4.psd1'
+    } else {
+        Join-Path $PSScriptRoot '..\..\MatterMostV4\MatterMostV4.psd1'
+    }
+    Import-Module $modulePath -Force
+
+        Connect-MMServer -Url $config.Url -Username $config.AdminUsername -Password (ConvertTo-SecureString $config.AdminPassword -AsPlainText -Force)
 
     $script:Suffix   = (Get-Date -Format 'HHmmss')
     $script:TestPass = ConvertTo-SecureString 'Pester123!' -AsPlainText -Force
